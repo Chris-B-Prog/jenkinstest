acc58031ab3dbba4d5c9d8e317cf20ebc8c09a3e
Chris-B-Prog <36234518+Chris-B-Prog@users.noreply.github.com>
2018-05-23 13:55:20 -0400

    Update s3test.tf

--- s3test.tf
+++ s3test.tf
@@ -1,3 +1,11 @@
+terraform {
+ backend "s3" {
+    bucket         = "pgr-terraform-state-046783355476"
+    key            = "jenkinstest-046783355476.tfstate"    region         = "us-east-1"
+    dynamodb_table = "pgr-terraform-lock-046783355476"
+  }
+}
+
 module "s3test" {
     source = "./tf_aws_s3"
     str_bucket = "pgr-cdb-jenkins-test"
